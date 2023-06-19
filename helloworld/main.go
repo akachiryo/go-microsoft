425764b90b264f41f4fca85ef7a997c29674f76f
akachiryo <85982768+akachiryo@users.noreply.github.com>
2023-06-19 23:50:36 +0900

    配列　スライス

--- helloworld/main.go
+++ helloworld/main.go
@@ -3,19 +3,10 @@ package main
 import "fmt"
 
 func main() {
-    val := 0
-
-    for {
-        fmt.Print("Enter number: ")
-        fmt.Scanf("%d", &val)
-
-        switch {
-        case val < 0:
-            panic("You entered a negative number!")
-        case val == 0:
-            fmt.Println("0 is neither negative nor positive")
-        default:
-            fmt.Println("You entered:", val)
-        }
+    student := map[string]int {
+        "a": 20,
+        "b": 30,
     }
+    delete(student, "b")
+    fmt.Println(student)
 }
